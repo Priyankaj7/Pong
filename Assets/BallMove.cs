85ff06b2c9b08c5296c02422258f226946419209
Priyankaj7 <31216698+Priyankaj7@users.noreply.github.com>
2017-08-21 21:45:53 +0530

    Add files via upload

---
+++ Assets/BallMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    
+    public float speed = 1f;
+    public Transform Ball;
+    public GameObject R;
+    public GameObject L;
+    public float x, y;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+    
+       
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(x, y)* Time.deltaTime);
+      
+        if (transform.position.x >= 8.5 && (transform.position.y <= R.transform.position.y + 2.5 && transform.position.y >= R.transform.position.y - 2.5))
+        {
+            x *= -1;
+        }
+        if (transform.position.x <= -8.5 && (transform.position.y <= L.transform.position.y + 2.5 && transform.position.y >= L.transform.position.y -2.5))
+        {
+            x *= -1;
+        }
+
+        if (transform.position.x >= 9 || transform.position.x <= -9)
+            transform.position = Vector3.zero;
+
+        if (transform.position.y > 6 || transform.position.y < -6)
+            y *= -1;
+    }
+    }
+
---
+++ Assets/Scripts/Player1move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1move : MonoBehaviour {
+
+    public Transform player1;
+    public float speed = 2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w"))
+        {
+            transform.Translate(new Vector3(0, 5, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(new Vector3(0, -5, 0) * Time.deltaTime);
+        }
+    }
+}
